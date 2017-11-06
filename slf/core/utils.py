f81592e8219f8259ac18ee9082acbddecc338c68
TomDonoghue <tdonoghue@ucsd.edu>
2017-11-06 12:19:03 -0800

    Update decorators: for each 1D & 2D

--- slf/core/utils.py
+++ slf/core/utils.py
@@ -75,11 +75,20 @@ def exclude_psd(freqs, psd, exclude):
 ################################################################################
 ################################################################################
 
-def CheckDims(func):
+def CheckDims1D(func):
+    """Decorator function to check all inputs are 1-D."""
+
+    def wrapper(*args):
+        args = [_check1D(arg) for arg in args]
+        return func(*args)
+
+    return wrapper
+
+def CheckDims2D(func):
     """Decorator function to check all inputs are 2-D."""
 
     def wrapper(*args):
-        args = [_check(arg) for arg in args]
+        args = [_check2D(arg) for arg in args]
         return func(*args)
 
     return wrapper
@@ -87,7 +96,15 @@ def CheckDims(func):
 ################################################################################
 ################################################################################
 
-def _check(arr):
+def _check1D(arr):
+    """Check that array is 1-D, squeeze if not."""
+
+    if arr.ndim == 2:
+        return np.squeeze(arr)
+    else:
+        return arr
+
+def _check2D(arr):
     """Check that array is 2-D, reshape if not."""
 
     if arr.ndim == 1:
