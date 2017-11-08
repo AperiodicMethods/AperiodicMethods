454d9bb739bd129e0104b1b2f22e1bbf47465599
TomDonoghue <tdonoghue@ucsd.edu>
2017-11-08 03:19:19 -0800

    Small fixes for full runs

--- scripts/run_syn_fits.py
+++ scripts/run_syn_fits.py
@@ -1,26 +1,33 @@
-"""   """
+"""Run script for fitting slope across synthetic PSDs, testing multiple methods."""
 
 from slf import syn
-from slf.fake import SynFits, print_errs
+from slf.fake import SynFits
+from slf.core.db import SLFDB
+from slf.core.io import save_pickle
 
 #########################################################################################
 #########################################################################################
 
 # PSD SETTINGS
-N_PSDS = 2
+N_PSDS = 100
 F_RANGE = [3, 40]
 
 # Simulation Settings
-NOISE_VALS = [0, 0.01, 0.05, 0.1, 0.15, 0.2]
-SLOPE_VALS = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+NOISE_VALS = [0.0, 0.01, 0.05, 0.1, 0.15, 0.2]
+SLOPE_VALS = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
 
 #########################################################################################
 #########################################################################################
 
 def main():
 
+    db = SLFDB()
+
     for slv in SLOPE_VALS:
         for nslv in NOISE_VALS:
+
+            # Print status
+            print('Running sims for slope val {}, noise-val {}'.format(slv, nslv))
 
             # Initialize syn-fitter object
             syns = SynFits()
@@ -32,9 +39,9 @@ def main():
             # Fit slopes
             syns.fit_slopes(slv, freqs, psds)
 
-            #
-            avgs = syns.calc_avg_errs()
-            print_errs(avgs)
+            # Save out fit data
+            save_name = 'SynFits_slv' + str(slv) + '_N' + str(nslv) + '.p'
+            save_pickle(syns.errs, save_name, db.syns_path)
 
 if __name__ == '__main__':
     main()
