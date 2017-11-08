f1bab991b7db9aa2a7dd10d0395f8e81fb8c8c3e
TomDonoghue <tdonoghue@ucsd.edu>
2017-11-07 22:39:39 -0800

    Fix up spacing

--- slf/plts/fake.py
+++ slf/plts/fake.py
@@ -7,31 +7,41 @@ import matplotlib.pyplot as plt
 ####################################################################################################
 
 def boxplot_errors(err_dists):
-	"""Plot a boxplot of distributions of errors for each slope fit method.
+    """Plot a boxplot of distributions of errors for each slope fit method.
 
-	Parameters
-	----------
-	err_dists : dict
-		Dictionary of error distributions, from SynFits object.
-	"""
+    Parameters
+    ----------
+    err_dists : dict
+        Dictionary of error distributions, from SynFits object.
+    """
 
-	fig = plt.figure(figsize=[10, 5])
-	plt.boxplot([err_dists[meth] for meth in err_dists.keys()],
-	                labels=err_dists.keys(),
-	                showfliers=False);
+    fig = plt.figure(figsize=[10, 5])
+    plt.boxplot([err_dists[meth] for meth in err_dists.keys()],
+                    labels=err_dists.keys(),
+                    showfliers=False);
+
 
 def violin_errors(err_dists):
-	"""Plot a violin plot of distributions of errors for each slope fit method.
+    """Plot a violin plot of distributions of errors for each slope fit method.
 
-	Parameters
-	----------
-	err_dists : dict
-		Dictionary of error distributions, from SynFits object.
-	"""
+    Parameters
+    ----------
+    err_dists : dict
+        Dictionary of error distributions, from SynFits object.
+    """
 
-	import seaborn as sns
+    import seaborn as sns
 
-	# Violin plot of the error distributions
-	df = pd.DataFrame(err_dists)
-	plt.figure(figsize=[16, 6])
-	ax = sns.violinplot(data=df, cut=0)
+    # Violin plot of the error distributions
+    df = pd.DataFrame(err_dists)
+    plt.figure(figsize=[16, 6])
+    ax = sns.violinplot(data=df, cut=0)
+
+####################################################################################################
+####################################################################################################
+
+def _color_red_or_green(val):
+    """Use colour code to visualize significant differences"""
+
+    color = 'green' if val < 0.05 else 'red'
+    return 'color: %s' % color
