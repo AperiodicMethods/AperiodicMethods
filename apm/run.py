fcc6581accce9cf38804f3f10b2a2f804749de6d
TomDonoghue <tdonoghue@ucsd.edu>
2019-09-12 07:59:56 +0200

    Add code for sim experiments

---
+++ apm/run.py
@@ -0,0 +1,47 @@
+"""Code for running simulation experiments."""
+
+import numpy as np
+
+###################################################################################################
+###################################################################################################
+
+upd_exp = lambda params, val : params.update({'exponent' : val})
+upd_freq = lambda params, val : params['components']['sim_oscillation'].update({'freq' : val})
+upd_pow = lambda params, val : params.update({'component_variances' : [1, val]})
+upd_comb_exp = lambda params, val : params['components']['sim_powerlaw'].update({'exponent' : val})
+
+
+UPDATES = {
+    'update_exponent' : upd_exp,
+    'update_frequency' : upd_freq,
+    'update_power' : upd_pow,
+    'update_comb_exp' : upd_comb_exp
+}
+
+
+def update_vals(sim_params, values, update):
+
+    for val in values:
+        update(sim_params, val)
+        yield sim_params
+
+
+def run_sims(sim_func, sim_params, measure_func, measure_params, n_instances=10,
+             values=np.arange(-3, 0.25, 0.25), update='update_exponent'):
+    """Run a set of calculated measures across simulations."""
+
+    outs = []
+
+    update = UPDATES[update] if isinstance(update, str) else update
+
+    for cur_sim_params in update_vals(sim_params.copy(), values, update):
+
+        inst_outs = []
+        for ind in range(n_instances):
+
+            sig = sim_func(**cur_sim_params)
+            inst_outs.append(measure_func(sig, **measure_params))
+
+        outs.append(np.mean(inst_outs))
+
+    return outs
