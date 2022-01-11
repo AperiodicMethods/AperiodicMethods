36112802d119aa3eb1e5e1cd4c1d2c0e991c2302
Tom Donoghue <tdonoghue.research@gmail.com>
2022-01-10 22:40:26 -0500

    add parallel runner, from ryan

--- apm/run.py
+++ apm/run.py
@@ -2,6 +2,10 @@
 
 from copy import deepcopy
 
+from multiprocessing import Pool, cpu_count
+from functools import partial
+from tqdm.notebook import tqdm
+
 import numpy as np
 
 ###################################################################################################
@@ -75,3 +79,50 @@ def run_sims(sim_func, sim_params, measure_func, measure_params, n_instances=10,
         outs.append(np.mean(inst_outs, 0))
 
     return outs
+
+
+def run_sims_parralel(sim_func, sim_params, measure_func, measure_params, n_instances=10,
+                      values=np.arange(-3, 0.25, 0.25), update='update_exp', n_jobs=-1):
+    """Compute a set of measures across simulations - in parallel."""
+
+    n_jobs = cpu_count() if n_jobs == -1 else n_jobs
+
+    # Generator to list
+    update = UPDATES[update] if isinstance(update, str) else update
+    sim_params = update_vals(deepcopy(sim_params), values, update)
+    sim_params = [deepcopy(next(sim_params)) for vi in values]
+
+    # Duplicate sims to equal length of n_instances
+    sim_params = [pii for pi in range(len(sim_params)) for pii in [sim_params[pi]] * n_instances]
+
+    with Pool(processes=n_jobs) as pool:
+
+        mapping = pool.imap(partial(_proxy, sim_func=sim_func, measure_func=measure_func,
+                                    measure_params=measure_params), sim_params)
+
+        measures = list(tqdm(mapping, desc="Running Simulations",
+                             total=len(sim_params), dynamic_ncols=True))
+
+    # Rehape array and take mean across n_instances
+    measures = np.array(measures)
+    remainder = int(measures.size / (len(values) * n_instances))
+
+    if remainder == 1:
+        # Cases when measure_func returns a single value
+        measures = np.reshape(measures, (len(values), n_instances))
+    else:
+        # Cases where measure_func returns >1 value
+        try:
+            measures = np.reshape(measures, (len(values), n_instances, remainder))
+        except:
+            raise ValueError('The measure function returns an array with varying shape.')
+
+    measures = np.mean(measures, axis=1)
+
+    return measures
+
+
+def _proxy(sim_params, sim_func=None, measure_func=None, measure_params=None):
+    """Wrap simulation and measure functions together."""
+
+    return measure_func(sim_func(**sim_params), **measure_params)
