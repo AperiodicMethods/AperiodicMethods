c1beceefe58a4587f537a4abc8a11f7120ccffa9
Tom Donoghue <tdonoghue.research@gmail.com>
2023-09-06 21:47:27 -0400

    add new run funcs

--- apm/run.py
+++ apm/run.py
@@ -1,6 +1,7 @@
 """Code for running simulation experiments."""
 
 from copy import deepcopy
+from itertools import product
 
 from multiprocessing import Pool, cpu_count
 from functools import partial
@@ -9,7 +10,7 @@ from tqdm.notebook import tqdm
 import numpy as np
 import pandas as pd
 
-from bootstrap import bootstrap_corr
+from bootstrap import bootstrap_corr, bootstrap_diff
 
 from apm.utils import unpack_param_dict
 
@@ -233,29 +234,29 @@ def run_measures(data, measures):
 
     Returns
     -------
-    outputs : dict
+    results : dict
         Output measures.
         The keys are labels for each applied method.
         The values are the computed measures for each method.
     """
 
-    # Initialize outputs
-    outputs = {func.__name__ : np.zeros(data.shape[0]) for func in measures.keys()}
+    # Initialize results
+    results = {func.__name__ : np.zeros(data.shape[0]) for func in measures.keys()}
 
     # Calculate measures on data
     for ind, sig in enumerate(data):
         for measure, params in measures.items():
-            outputs[measure.__name__][ind] = measure(sig, **params)
+            results[measure.__name__][ind] = measure(sig, **params)
 
-    return outputs
+    return results
 
 
-def compute_all_corrs(outputs, select=None, corr_func=bootstrap_corr):
+def compute_all_corrs(results, select=None, corr_func=bootstrap_corr):
     """Compute correlations across all sets of measures.
 
     Parameters
     ----------
-    outputs : dict
+    results : dict
         Measure results.
         Each key should be a measure name.
         Each set of values should be an array of measure results.
@@ -270,26 +271,90 @@ def compute_all_corrs(outputs, select=None, corr_func=bootstrap_corr):
         Each value is another dictionary, with the measure names and correlation results of all other measures.
     """
 
-    methods = outputs.keys()
-
+    methods = results.keys()
     all_corrs = {method : {} for method in methods}
 
-    for m1 in methods:
-        for m2 in methods:
+    for m1, m2 in product(methods, methods):
 
-            if m1 == m2:
-                continue
-            try:
-                all_corrs[m2][m1]
-            except KeyError:
+        # Skip if m's are same, or if results already in output
+        if m1 == m2 or all_corrs.get(m2).get(m1) is not None:
+            continue
 
-                if select is not None:
-                    d1, d2 = outputs[m1][select], outputs[m2][select]
-                else:
-                    d1, d2 = outputs[m1], outputs[m2]
+        if select is not None:
+            d1, d2 = results[m1][select], results[m2][select]
+        else:
+            d1, d2 = results[m1], results[m2]
 
-                corrs = corr_func(d1, d2)
-                all_corrs[m1][m2] = corrs
-                all_corrs[m2][m1] = corrs
+        corrs = corr_func(d1, d2)
+        all_corrs[m1][m2] = corrs
+        all_corrs[m2][m1] = corrs
 
     return all_corrs
+
+
+def compute_corrs_to_feature(results, feature, corr_func=bootstrap_corr):
+    """Compute correlations between a set of measures and a given feature.
+
+    Parameters
+    ----------
+    results : dict
+        Measure results.
+        Each key should be a measure name.
+        Each set of values should be an array of measure results.
+    feature : 1d array
+        Vector of values to computer correlations to.
+        Should have the same length as each entry in `results`.
+
+    Returns
+    -------
+    all_corrs : dict
+        Correlation results.
+        Each key is a measure name.
+        Each value is the correlation results between this measure and the given feature.
+    """
+
+    methods = results.keys()
+
+    all_corrs = {method : None for method in methods}
+
+    for method in methods:
+        all_corrs[method] = corr_func(results[method], feature)
+
+    return all_corrs
+
+
+def compute_diffs_to_feature(results, feature, diff_func=bootstrap_diff):
+    """Compute differences between correlations of a set of measures to a given feature.
+
+    Parameters
+    ----------
+    results : dict
+        Measure results.
+        Each key should be a measure name.
+        Each set of values should be an array of measure results.
+    feature : 1d array
+        Vector of values to computer correlations to.
+        Should have the same length as each entry in `results`.
+
+    Results
+    -------
+    all_diffs : dict
+        Difference results.
+        Each key is a measure name.
+        Each value is another dictionary, with the measure names and difference results to all other measures.
+    """
+
+    methods = results.keys()
+    all_diffs = {method : {} for method in methods}
+
+    for m1, m2 in product(methods, methods):
+
+        # Skip if m's are same, or if results already in output
+        if m1 == m2 or all_diffs.get(m2).get(m1) is not None:
+            continue
+
+        diffs = diff_func(feature, results[m1], results[m2])
+        all_diffs[m1][m2] = diffs
+        all_diffs[m2][m1] = diffs
+
+    return all_diffs
