c756006bdc1c3d0e219288e77b3bce0f833e254c
Tom Donoghue <tdonoghue.research@gmail.com>
2023-09-11 17:59:53 -0400

    udpate sim settings - add knee stuff

--- apm/sim/params.py
+++ apm/sim/params.py
@@ -11,6 +11,9 @@ upd_exp = lambda params, val : params.update({'exponent' : val})
 # Update aperiodic parameters (in combined signals) - exponent
 upd_comb_exp = lambda params, val : params['components']['sim_powerlaw'].update({'exponent' : val})
 
+# Update aperiodic parameters - knee
+upd_knee = lambda params, val : params.update({'tau_d' : val})
+
 # Update periodic parameters (in combined signals)
 upd_freq = lambda params, val : params['components']['sim_oscillation'].update({'freq' : val})
 upd_pow = lambda params, val : params.update({'component_variances' : [1, val]})
@@ -18,6 +21,7 @@ upd_pow = lambda params, val : params.update({'component_variances' : [1, val]})
 UPDATES = {
     'update_exp' : upd_exp,
     'update_comb_exp' : upd_comb_exp,
+    'update_knee' : upd_knee,
     'update_freq' : upd_freq,
     'update_pow' : upd_pow,
 }
--- apm/sim/settings.py
+++ apm/sim/settings.py
@@ -5,20 +5,40 @@ import numpy as np
 ###################################################################################################
 ###################################################################################################
 
+# Set the number of instances to run
+N_SIMS = 50
+
 # General simulation settings
 N_SECONDS = 30
 FS = 500
 
+# Define aperiodic parameters
 EXP = -1.5
+KNEE =
+
+# Define additional parameters related to aperiodic components
 F_RANGE = (0.5, None)
+
+# Define periodic parameters
 FREQ = 10
 BW = 1.5
 HEIGHT = 1.5
 
+# Define parameters for combined signals
 COMP_VARS = [1, 0.25]
+
+# Aperiodic parameters - ranges
+EXPS = np.arange(-3, 0.5, 0.5)
+KNEES = np.array([0.002, 0.005, 0.015, 0.040, 0.070])
+
+# Periodic parameter - ranges
+FREQS = np.arange(1, 50, 2)
+POWERS = np.arange(0, 2, 0.1)
+BWS = np.arange(0, 2.0, 0.5)
 
 # Collect together simulation parameters
 SIM_PARAMS_AP = {'n_seconds' : N_SECONDS, 'fs' : FS, 'f_range' : F_RANGE}
+SIM_PARAMS_KNEE = {'n_seconds' : N_SECONDS, 'fs' : FS}
 SIM_PARAMS_COMB = {'n_seconds' : N_SECONDS, 'fs' : FS,
                    'components' : {'sim_powerlaw' : {'exponent' : EXP, 'f_range' : F_RANGE},
                                    'sim_oscillation' : {'freq' : FREQ}},
@@ -28,11 +48,3 @@ SIM_PARAMS_BURST = {'n_seconds' : N_SECONDS, 'fs' : FS,
                                     'sim_bursty_oscillation' : {'freq' : FREQ}},
                     'component_variances' : COMP_VARS}
 SIM_PARAMS_PEAK = {'freq' : FREQ, 'bw' : BW, 'height' : HEIGHT}
-
-# Set the number of instances to run
-N_SIMS = 50
-
-# Set the range of exponents to explore
-EXPS = np.arange(-3, 0.5, 0.5)
-FREQS = np.arange(1, 50, 2)
-POWERS = np.arange(0, 2, 0.1)
