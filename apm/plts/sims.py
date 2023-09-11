e2bdbc7b8b003784519ccd4310028eda10576592
Tom Donoghue <tdonoghue.research@gmail.com>
2023-09-11 18:14:54 -0400

    update plot_pe_sims for expected

--- apm/plts/sims.py
+++ apm/plts/sims.py
@@ -27,14 +27,24 @@ def plot_ap_sims(sims_exp, sims_exp_var, sims_comb, sims_comb_var, ylabel=None, 
 
 
 @savefig
-def plot_pe_sims(sims_freq, sims_freq_var, sims_pow, sims_pow_var, ylabel=None):
+def plot_pe_sims(sims_freq, sims_freq_var, sims_pow, sims_pow_var, ylabel=None, expected=None):
     """Plot simulations across periodic parameters."""
 
     _, ax = plt.subplots(figsize=(6, 5))
+
     plot_lines(FREQS, sims_freq, sims_freq_var,
                color=FREQ_COLOR, xlabel='Oscillation Frequency', ylabel=ylabel, ax=ax)
+
     ax2 = ax.twiny()
     plot_lines(POWERS, sims_pow, sims_pow_var,
                color=POW_COLOR, xlabel='Oscillation Power', ax=ax2)
-    plt.legend([ax.get_lines()[0], ax2.get_lines()[0]],
-               ['Frequency', 'Power'], loc=4, fontsize=14)
+
+    leg_lines = [ax.get_lines()[0], ax2.get_lines()[0]]
+    leg_labels = ['Frequency', 'Power']
+
+    if expected is not None:
+        plot_lines(FREQS, expected, color='k', linestyle='--', ax=ax)
+        leg_lines.append(ax.get_lines()[1])
+        leg_labels.append('Expected')
+
+    plt.legend(leg_lines, leg_labels, loc=4, fontsize=14)
