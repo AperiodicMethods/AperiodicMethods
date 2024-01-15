fffbab65b2eac71a0f81f8992ce912a5e8aa1fb8
Tom Donoghue <tdonoghue.research@gmail.com>
2024-01-15 11:26:06 -0500

    reorg run funcs

---
+++ apm/run/__init__.py
@@ -0,0 +1,4 @@
+"""Code for running method estimations for the Aperiodic Methods project."""
+
+from .sims import run_sims, run_sims_load, run_sims_parallel, run_comparisons
+from .data import run_measures, run_group_measures
---
+++ apm/run/data.py
@@ -0,0 +1,64 @@
+"""Code for running estimates on empirical data."""
+
+import warnings
+
+import numpy as np
+
+###################################################################################################
+###################################################################################################
+
+def run_measures(data, measures, warnings_action='ignore'):
+    """Compute multiple measures on empirical recordings - 2D array input.
+
+    Parameters
+    ----------
+    data : 2d array
+        Data to run measures on, organized as [channels, timepoints].
+    measures : dict
+        Functions to apply to the data.
+        The keys should be functions to apply to the data.
+        The values should be a dictionary of parameters to use for the method.
+
+    Returns
+    -------
+    results : dict
+        Output measures.
+        The keys are labels for each applied method.
+        The values are the computed measures for each method.
+    """
+
+    # Initialize results
+    results = {func.__name__ : np.zeros(data.shape[0]) for func in measures.keys()}
+
+    # Calculate measures on data
+    with warnings.catch_warnings():
+        warnings.simplefilter(warnings_action)
+        for ind, sig in enumerate(data):
+            for measure, params in measures.items():
+                results[measure.__name__][ind] = measure(sig, **params)
+
+    return results
+
+
+def run_group_measures(group_data, measures):
+    """Compute multiple measures on empirical recordings - 3D array input.
+
+    Parameters
+    ----------
+    group_data : 3d array
+        Data to run measures on, organized as [subjects, channels, timepoints].
+    measures : dict
+        Functions to apply to the data.
+        The keys should be functions to apply to the data.
+        The values should be a dictionary of parameters to use for the method.
+    """
+
+    n_subjs, n_chs, n_timepoints = group_data.shape
+    group_results = {func.__name__ : np.zeros([n_subjs, n_chs]) for func in measures.keys()}
+
+    for sub_ind in range(n_subjs):
+        subj_measures = run_measures(np.squeeze(group_data[sub_ind, :, :]), measures)
+        for label in subj_measures:
+            group_results[label][sub_ind, :] = subj_measures[label]
+
+    return group_results
