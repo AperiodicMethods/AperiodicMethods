fc99cc6ef37b09405c85d76471a94063fe2d4128
Tom Donoghue <tdonoghue.research@gmail.com>
2024-02-09 00:29:02 -0500

    update ieeg processing - two fit ranges

--- scripts/analyze_ieeg.py
+++ scripts/analyze_ieeg.py
@@ -15,11 +15,11 @@ sys.path.append(str(Path('..').resolve()))
 from apm.io.data import load_ieeg_all
 from apm.io import APMDB, get_files, save_pickle
 from apm.run import run_measures
-from apm.methods import fit_irasa_knee
+from apm.methods import fit_irasa_exp, fit_irasa_knee
 from apm.analysis import compute_all_corrs
 
 # Import general settings from script settings
-from settings import TS_MEASURES, SPECPARAM_SETTINGS_KNEE
+from settings import TS_MEASURES, SPECPARAM_SETTINGS, SPECPARAM_SETTINGS_KNEE
 
 ###################################################################################################
 ###################################################################################################
@@ -54,7 +54,8 @@ MAX_TIME = 30
 
 ## METHOD SETTINGS
 
-FIT_RANGE = [1, 60]
+FIT_RANGE_SHORT = [3, 40]
+FIT_RANGE_LONG = [1, 60]
 
 PSD_SETTINGS = {
     'fs' : FS,
@@ -62,9 +63,15 @@ PSD_SETTINGS = {
     'noverlap' : FS,
 }
 
-IRASA_SETTINGS = {
+IRASA_SETTINGS_SHORT = {
     'fs' : FS,
-    'f_range' : FIT_RANGE,
+    'f_range' : FIT_RANGE_SHORT,
+}
+
+
+IRASA_SETTINGS_LONG = {
+    'fs' : FS,
+    'f_range' : FIT_RANGE_LONG,
 }
 
 ###################################################################################################
@@ -87,31 +94,48 @@ def main():
     # Compute time domain measures of interest on the iEEG data
     results = run_measures(all_data, TS_MEASURES)
 
-    # Run specparam
+    # Compute power spectra
     freqs, powers = compute_spectrum(all_data, **PSD_SETTINGS)
-    fg = FOOOFGroup(**SPECPARAM_SETTINGS_KNEE)
-    fg.fit(freqs, powers, FIT_RANGE)
-    fg.save('ieeg_specparam', OUTPATH, save_results=True)
 
-    # Add specparam measures to overall results
-    results['specparam'] = np.array(fg.get_params('aperiodic', 'exponent'))
+    # Run specparam - short range
+    fg = FOOOFGroup(**SPECPARAM_SETTINGS)
+    fg.fit(freqs, powers, FIT_RANGE_SHORT)
+    fg.save('ieeg_specparam_short', OUTPATH, save_results=True)
+
+    # Add specparam measures to overall results - short
+    results['specparam_short'] = np.array(fg.get_params('aperiodic', 'exponent'))
+
+    # Run specparam - long range
+    fg = FOOOFGroup(**SPECPARAM_SETTINGS_KNEE)
+    fg.fit(freqs, powers, FIT_RANGE_LONG)
+    fg.save('ieeg_specparam_long', OUTPATH, save_results=True)
+
+    # Add specparam measures to overall results - long
+    results['specparam_long'] = np.array(fg.get_params('aperiodic', 'exponent'))
     knee_freqs = [compute_knee_frequency(kn, exp) \
         for kn, exp in zip(fg.get_params('aperiodic', 'knee'),
                            fg.get_params('aperiodic', 'exponent'))]
     results['specparam_knee'] = np.array(fg.get_params('aperiodic', 'knee'))
     results['specparam_knee_freq'] = np.nan_to_num(np.array(knee_freqs))
 
-    # Run IRASA
+    # Run IRASA and add results to overall results - short
+    ir_exps = []
+    for sig in all_data:
+        freqs_ir, psd_ap, psd_pe = compute_irasa(sig, **IRASA_SETTINGS_SHORT)
+        ir_exps.append(-fit_irasa_exp(freqs_ir, psd_ap))
+    results['irasa_short'] = np.array(ir_exps)
+
+    # Run IRASA - long
     ir_exps = []
     ir_knees = []
     for sig in all_data:
-        freqs_ir, psd_ap, psd_pe = compute_irasa(sig, **IRASA_SETTINGS)
+        freqs_ir, psd_ap, psd_pe = compute_irasa(sig, **IRASA_SETTINGS_LONG)
         off_ir, kne_ir, exp_ir = fit_irasa_knee(freqs_ir, psd_ap)
         ir_exps.append(-exp_ir)
         ir_knees.append(kne_ir)
 
     # Add IRASA results to overall results
-    results['irasa'] = np.array(ir_exps)
+    results['irasa_long'] = np.array(ir_exps)
     knee_freqs_ir = [compute_knee_frequency(kn, exp) for kn, exp in zip(ir_knees, ir_exps)]
     results['irasa_knee'] = np.array(ir_knees)
     results['irasa_knee_freq'] = np.nan_to_num(np.array(knee_freqs_ir))
