b44582a8748fd04cc60a3df1e5975b2d12615a28
Tom Donoghue <tdonoghue.research@gmail.com>
2024-02-14 23:25:09 -0500

    udpate helper funcs for printing corrs

--- apm/utils/__init__.py
+++ apm/utils/__init__.py
@@ -1,3 +1,3 @@
-"""Utilities for the Aperiodic Methods toolbox."""
+"""Utilities for the Aperiodic Methods project."""
 
-from .format import print_results, format_corr
+from .format import print_results, format_corr, print_all_corrs, print_corr_combs
--- apm/utils/format.py
+++ apm/utils/format.py
@@ -1,5 +1,9 @@
 """Utility functions for formatting outputs."""
 
+from itertools import combinations
+
+from apm.plts.settings import ABBRS
+
 ###################################################################################################
 ###################################################################################################
 
@@ -20,3 +24,25 @@ def format_corr(r_val, p_val, cis):
     """Format correlation results for printing."""
 
     return 'r={:+1.3f}  CI[{:+1.3f}, {:+1.3f}],  p={:1.3f}'.format(r_val, *cis, p_val)
+
+
+def print_all_corrs(corrs, rows, columns):
+    """Print out a set of correlations defined as a two-dimensional sweep."""
+
+    print('Correlations:')
+    for row in rows:
+        for col in columns:
+            print('  {:6s} & {:6s}:  '.format(ABBRS[row], ABBRS[col]),
+                  format_corr(*corrs[row][col]))
+
+
+def print_corr_combs(corrs, combs=None):
+    """Print correlations, stepping through all combinations."""
+
+    if not combs:
+        combs = list(corrs.keys())
+
+    print('Correlations:')
+    for comb in combinations(combs, 2):
+        print('  {:6s} & {:6s}:  '.format(ABBRS[comb[0]], ABBRS[comb[1]]),
+              format_corr(*corrs[comb[0]][comb[1]]))
