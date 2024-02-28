0356ae9eb673bd7076304b7e8e212c24bec48dd8
Tom Donoghue <tdonoghue.research@gmail.com>
2024-02-28 17:45:54 -0500

    update create_sims for new appraoch

--- scripts/create_sims.py
+++ scripts/create_sims.py
@@ -5,7 +5,7 @@ And/or we might want to test multiple different analyses on the exact same set o
 To support this, here we pre-generate simulations that can be reloaded when needed.
 """
 
-from neurodsp.sim import sim_powerlaw, sim_combined, sim_synaptic_current
+from neurodsp.sim import sim_powerlaw, sim_combined, sim_synaptic_current, sim_knee
 
 # Import custom code
 import sys; from pathlib import Path
@@ -13,8 +13,8 @@ sys.path.append(str(Path('..').resolve()))
 from apm.io import APMDB, save_pickle
 from apm.sim import sim_across_values
 from apm.sim.sim import sim_combined_peak
-from apm.sim.settings import (N_SIMS, EXPS, KNEES, FREQS, POWERS, BWS,
-                              SIM_PARAMS_AP, SIM_PARAMS_COMB, SIM_PARAMS_KNEE, SIM_PARAMS_PEAK)
+from apm.sim.defs import SIM_ITERS
+from apm.sim.settings import N_SIMS
 
 ###################################################################################################
 ###################################################################################################
@@ -31,39 +31,43 @@ def main():
 
     # Simulate a predefined set of exponent variation simulations
     print('\tcreating exponent simulations...')
-    sigs_ap = sim_across_values(\
-        sim_powerlaw, SIM_PARAMS_AP, 'update_exp', EXPS, N_SIMS)
+    sigs_ap = sim_across_values(sim_powerlaw, SIM_ITERS['ap_exp'], N_SIMS)
     save_pickle(sigs_ap, 'ts_sims_ap_exp', SIMPATH)
 
     # Simulate a predefined set of combined exponent variation simulations
     print('\tcreating combined simulations...')
-    sigs_comb = sim_across_values(\
-        sim_combined, SIM_PARAMS_COMB, 'update_comb_exp', EXPS, N_SIMS)
+    sigs_comb = sim_across_values(sim_combined, SIM_ITERS['comb_exp'], N_SIMS)
     save_pickle(sigs_comb, 'ts_sims_comb_exp', SIMPATH)
 
     # Simulate a predefined set of combined frequency variation simulations
     print('\tcreating frequency simulations...')
-    sigs_freq = sim_across_values(\
-        sim_combined, SIM_PARAMS_COMB, 'update_freq', FREQS, N_SIMS)
+    sigs_freq = sim_across_values(sim_combined, SIM_ITERS['osc_freq'], N_SIMS)
     save_pickle(sigs_freq, 'ts_sims_comb_freq', SIMPATH)
 
     # Simulate a predefined set of combined oscillation power simulations
     print('\tcreating power simulations...')
-    sigs_pow = sim_across_values(\
-        sim_combined, SIM_PARAMS_COMB, 'update_pow', POWERS, N_SIMS)
+    sigs_pow = sim_across_values(sim_combined, SIM_ITERS['osc_pow'], N_SIMS)
     save_pickle(sigs_pow, 'ts_sims_comb_pow', SIMPATH)
+
+    # Simulate a predefined set of synaptic timescale variation simulations
+    print('\tcreating synaptic timescale simulations...')
+    sigs_tscales = sim_across_values(sim_synaptic_current, SIM_ITERS['syn_tscales'], N_SIMS)
+    save_pickle(sigs_tscales, 'ts_sims_ap_tscales', SIMPATH)
 
     # Simulate a predefined set of aperiodic knee variation simulations
     print('\tcreating knee simulations...')
-    sigs_knee = sim_across_values(\
-        sim_synaptic_current, SIM_PARAMS_KNEE, 'update_knee', KNEES, N_SIMS)
+    sigs_knee = sim_across_values(sim_knee, SIM_ITERS['kn_knee'], N_SIMS)
     save_pickle(sigs_knee, 'ts_sims_ap_knee', SIMPATH)
 
     # Simulate a predefined set of combined peak bandwidth variation simulations
     print('\tcreating bandwidth simulations...')
-    sigs_bw = sim_across_values(\
-        sim_combined_peak, SIM_PARAMS_PEAK, 'update_peak_bw', BWS, N_SIMS)
+    sigs_bw = sim_across_values(sim_combined_peak, SIM_ITERS['peak_bw'], N_SIMS)
     save_pickle(sigs_bw, 'ts_sims_comb_bw', SIMPATH)
+
+    # Simulate a predefined set of bursty oscillation simulations
+    print('\tcreating bursty simulations...')
+    sigs_burst = sim_across_values(sim_combined, SIM_ITERS['comb_burst'], N_SIMS)
+    save_pickle(sigs_burst, 'ts_sims_comb_burst', SIMPATH)
 
     print('\nSIMULATIONS CREATED\n')
 
